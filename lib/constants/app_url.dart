bd236cf96720fd47aebf0f6c6e1d613a59f7b1d4
Cristian Bonilla <cristiancamilo10_95@outlook.com>
2025-05-14 09:34:57 -0500

    :wrench: Constant to define the app URLs

---
+++ lib/constants/app_url.dart
@@ -0,0 +1,8 @@
+final class AppUrl {
+  static const String _host = '10.0.2.2';
+  static const String baseUrl = '$_host:22163';
+  static const String _extendedUrl = '/api/v1';
+  static final String teacherUrl = '$_extendedUrl/teacher';
+  static final String studentUrl = '$_extendedUrl/student';
+  static final String subjectUrl = '$_extendedUrl/subject';
+}
